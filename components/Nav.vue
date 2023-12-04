d8a7db901751dee196cfb5a82137cc8066ca7b55
Dagim_Shimelis <dagimshimelis1992@gmail.com>
2023-12-04 23:21:18 +0300

    [MOD] Made Navigation mobile responsive

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,5 +1,5 @@
 <script setup>
-    const links = ref([
+    const navigation = ref([
         {
             name: "home",
             to: "#home",
@@ -21,32 +21,107 @@
             to: "#contact",
         },
     ]);
+
+    const visibleSideNav = ref(false);
 </script>
 
 <template>
-    <div class="relative">
-        <div class="side-space !h-20 !z-50 flex items-center">
+    <header class="relative">
+        <div class="side-space !h-20 !z-50 flex items-center justify-between">
             <!-- Logo -->
             <div class="absolute top-4">
                 <img
                     src="/icons/logo.png"
                     alt="logo"
+                    class="w-10 lg:w-fit"
+                    draggable="false"
                 />
             </div>
-            <!-- Links -->
-            <ul class="flex items-center gap-x-16 w-fit mx-auto">
+            <!-- /* ------------------------------- Desktop Nav ------------------------------ */ -->
+            <ul class="hidden lg:flex items-center gap-x-16 w-fit mx-auto">
                 <li
-                    v-for="(link, i) in links"
+                    v-for="(navigator, i) in navigation"
                     :key="i"
-                    class="uppercase font-[400] tracking-widest hover:scale-110 ease-in-out duration-150"
+                    class="nav-link cursor-custom-pointer"
                 >
                     <router-link
-                        :to="link.to"
-                        class="cursor-custom-pointer"
-                        >{{ link.name }}</router-link
+                        :to="navigator.to"
+                        class=""
+                        >{{ navigator.name }}</router-link
                     >
                 </li>
             </ul>
+            <!-- /* ------------------------------- Mobile Nav ------------------------------- */ -->
+            <div class="lg:hidden ml-auto">
+                <button
+                    @click="visibleSideNav = true"
+                    class="menu-button absolute top-5 right-5 z-50"
+                >
+                    <Icon
+                        name="uil:bars"
+                        class="text-2xl"
+                    />
+                </button>
+                <Sidebar
+                    v-model:visible="visibleSideNav"
+                    position="right"
+                    class="relative w-full"
+                >
+                    <template #header>
+                        <button
+                            @click="visibleSideNav = false"
+                            class="menu-button absolute top-5 right-5 z-10"
+                        >
+                            <Icon
+                                name="uil:multiply"
+                                class="text-2xl"
+                            />
+                        </button>
+                    </template>
+                    <nav
+                        class="absolute top-0 right-0 ml-auto w-full h-full bg-black p-6 flex flex-col justify-center gap-y-10"
+                    >
+                        <NuxtLink
+                            @click="visibleSideNav = false"
+                            to="/"
+                            class="w-fit h-fit mx-auto z-30 mt-20"
+                        >
+                            <img
+                                src="/icons/logo.png"
+                                alt="logo"
+                                draggable="false"
+                            />
+                        </NuxtLink>
+                        <ul class="z-30 flex flex-col text-center gap-y-[59px]">
+                            <li
+                                v-for="(navigator, i) in navigation"
+                                :key="i"
+                            >
+                                <NuxtLink
+                                    @click="visibleSideNav = false"
+                                    :to="navigator.to"
+                                    class="nav-link cursor-custom-pointer"
+                                >
+                                    <p>{{ navigator.name }}</p></NuxtLink
+                                >
+                            </li>
+                        </ul>
+                        <!-- Graphic line -->
+                        <!-- <img
+                            src="/images/graphic-lines/crossed-line.png"
+                            alt=""
+                            class="absolute top-5 -left-5 z-20 -scale-y-100 -rotate-90"
+                            draggable="false"
+                        />
+                        <img
+                            src="/images/graphic-lines/ellipse-line.png"
+                            alt=""
+                            class="absolute bottom-0 left-0 z-20 scale-x-100"
+                            draggable="false"
+                        /> -->
+                    </nav>
+                </Sidebar>
+            </div>
         </div>
 
         <!-- /* ------------------------------ Graphic lines ----------------------------- */ -->
@@ -57,7 +132,15 @@
                 :draggable="false"
             />
         </div>
-    </div>
+    </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+    .nav-link {
+        @apply z-10 text-white uppercase font-[400] tracking-widest hover:scale-110 ease-in-out duration-150;
+        mix-blend-mode: difference;
+    }
+    .menu-button {
+        @apply p-2 aspect-square rounded-lg bg-white/10 text-white;
+    }
+</style>
--- plugins/primevue.js
+++ plugins/primevue.js
@@ -2,6 +2,8 @@ import PrimeVue from "primevue/config";
 
 import Button from "primevue/button";
 import Panel from "primevue/panel";
+import Sidebar from "primevue/sidebar";
+
 import Tailwind from "primevue/passthrough/tailwind";
 
 export default defineNuxtPlugin((nuxtApp) => {
@@ -9,14 +11,17 @@ export default defineNuxtPlugin((nuxtApp) => {
     app.use(PrimeVue, {
         pt: {
             button: {
-                root: { class: 'bg-teal-500 hover:bg-teal-700 text-white p-3 border-round border-none flex gap-2' },
-                label: 'text-white font-bold text-xl', // OR { class: 'text-white font-bold text-xl' }
-                icon: 'text-white text-2xl'
-            }
-        }
+                root: {
+                    class: "bg-teal-500 hover:bg-teal-700 text-white p-3 border-round border-none flex gap-2",
+                },
+                label: "text-white font-bold text-xl", // OR { class: 'text-white font-bold text-xl' }
+                icon: "text-white text-2xl",
+            },
+        },
     });
 
     // components
     app.component("Button", Button);
     app.component("Panel", Panel);
+    app.component("Sidebar", Sidebar);
 });
