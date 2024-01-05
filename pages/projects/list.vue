25e5c57a730f22fe91098208d2698cec4440cd68
Dagim_Shimelis <dagimshimelis1992@gmail.com>
2024-01-05 21:27:00 +0300

    [MOD] Make project list page mobile responsive

--- pages/projects/list.vue
+++ pages/projects/list.vue
@@ -30,7 +30,7 @@
 <template>
     <div class="relative min-h-screen pt-24 !pb-[300px]">
         <div class="side-space">
-            <div class="title-box w-fit !mx-auto">
+            <div class="title-box w-fit lg:!mx-auto">
                 <p class="font-[200]">Some of the</p>
                 <div>
                     <h1 class="title-big">projects</h1>
@@ -42,18 +42,18 @@
                 </div>
                 <p class="font-[200]">I've worked on</p>
             </div>
-            <p class="mt-10 text-center mx-auto w-[800px] font-[200] leading-8">
+            <p class="mt-10 text-left lg:text-center mx-auto lg:w-[800px] font-[200] leading-8">
                 As you explore the projects, you'll see the diverse range of
                 work I've undertaken, from dynamic web apps to static landing
                 pages. Each project tells a unique story of challenges met,
                 problems solved, and innovation realized.
             </p>
-            <div class="mt-20 grid grid-cols-2 gap-x-14 gap-y-28">
+            <div class="mt-20 grid md:grid-cols-2 gap-14 md:gap-y-28">
                 <router-link
                     v-for="(project, i) in projects"
                     :key="i"
                     :to="project.link"
-                    class="cursor-custom-pointer border border-transparent hover:border-white/5 h-full p-2 pb-6 rounded-[20px]"
+                    class="cursor-custom-pointer border border-white/5 md:border-transparent hover:border-white/5 h-full p-2 pb-6 rounded-[20px]"
                 >
                     <img
                         :src="project.thumbnailImage"
