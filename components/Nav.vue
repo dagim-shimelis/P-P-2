8e778aeb8708c87d9e1524128acd4fee37c6adfe
Dagim_Shimelis <dagimshimelis1992@gmail.com>
2024-01-07 21:08:45 +0300

    [MOD] Modified Nav Header
    - Make Nav fixed
    - Add gradient

--- components/Nav.vue
+++ components/Nav.vue
@@ -37,119 +37,125 @@
 <template>
     <header
         id="home"
-        class="relative"
+        class="relative !h-20"
     >
         <div
-            class="side-space !h-20 !z-50 flex items-center justify-between !pt-10"
+            class="fixed w-full !h-20 !z-50 from-black  via-black/90 via-30% to-transparent to-90% bg-gradient-to-b"
         >
-            <!-- Logo -->
-            <router-link
-                to="/"
-                class="absolute top-2 sm:top-4"
+            <div
+                class="side-space !h-20 flex items-center justify-between !pt-2"
             >
-                <img
-                    src="/icons/logo.png"
-                    alt="logo"
-                    class="w-10 lg:w-[60px]"
-                    draggable="false"
-                />
-            </router-link>
-            <!-- /* ------------------------------- Desktop Nav ------------------------------ */ -->
-            <ul class="hidden lg:flex items-center gap-x-16 w-fit mx-auto">
-                <li
-                    v-for="(navigator, i) in navigation"
-                    :key="i"
-                    class="nav-link"
+                <!-- Logo -->
+                <router-link
+                    to="/"
+                    class="absolute top-2 sm:top-4"
                 >
-                    <router-link
-                        :to="navigator.to"
-                        class="cursor-custom-pointer"
-                        >{{ navigator.name }}</router-link
-                    >
-                </li>
-            </ul>
-            <!-- <button class="cursor-custom-pointer z-30">
-                <Icon
-                    name="solar:file-download-outline"
-                    class="hidden lg:block w-6 h-6"
-                />
-            </button> -->
-            <div></div>
-            <!-- /* ------------------------------- Mobile Nav ------------------------------- */ -->
-            <div class="lg:hidden ml-auto">
-                <button
-                    @click="visibleSideNav = true"
-                    class="menu-button absolute top-3 sm:top-5 right-4 sm:right-8 z-10"
-                >
-                    <Icon
-                        name="uil:bars"
-                        class="text-2xl"
+                    <img
+                        src="/icons/logo.png"
+                        alt="logo"
+                        class="w-10 lg:w-[60px]"
+                        draggable="false"
                     />
-                </button>
-                <Sidebar
-                    v-model:visible="visibleSideNav"
-                    position="right"
-                    class="relative w-full"
-                >
-                    <template #header>
-                        <button
-                            @click="visibleSideNav = false"
-                            class="menu-button absolute top-3 sm:top-5 right-4 sm:right-8 z-10"
-                        >
-                            <Icon
-                                name="uil:multiply"
-                                class="text-2xl"
-                            />
-                        </button>
-                    </template>
-                    <nav
-                        class="absolute top-0 right-0 ml-auto w-full h-full bg-black p-6 flex flex-col justify-center gap-y-10"
+                </router-link>
+                <!-- /* ------------------------------- Desktop Nav ------------------------------ */ -->
+                <ul class="hidden lg:flex items-center gap-x-16 w-fit mx-auto">
+                    <li
+                        v-for="(navigator, i) in navigation"
+                        :key="i"
+                        class="nav-link"
                     >
-                        <NuxtLink
-                            @click="visibleSideNav = false"
-                            to="/"
-                            class="w-fit h-fit mx-auto z-30 mt-20"
+                        <router-link
+                            :to="navigator.to"
+                            class="cursor-custom-pointer"
+                            >{{ navigator.name }}</router-link
                         >
-                            <img
-                                src="/icons/logo.png"
-                                alt="logo"
+                    </li>
+                </ul>
+                <!-- <button class="cursor-custom-pointer z-30">
+                    <Icon
+                        name="solar:file-download-outline"
+                        class="hidden lg:block w-6 h-6"
+                    />
+                </button> -->
+                <div></div>
+                <!-- /* ------------------------------- Mobile Nav ------------------------------- */ -->
+                <div class="lg:hidden ml-auto">
+                    <button
+                        @click="visibleSideNav = true"
+                        class="menu-button absolute top-3 sm:top-5 right-4 sm:right-8 z-10"
+                    >
+                        <Icon
+                            name="uil:bars"
+                            class="text-2xl"
+                        />
+                    </button>
+                    <Sidebar
+                        v-model:visible="visibleSideNav"
+                        position="right"
+                        class="relative w-full"
+                    >
+                        <template #header>
+                            <button
+                                @click="visibleSideNav = false"
+                                class="menu-button absolute top-3 sm:top-5 right-4 sm:right-8 z-10"
+                            >
+                                <Icon
+                                    name="uil:multiply"
+                                    class="text-2xl"
+                                />
+                            </button>
+                        </template>
+                        <nav
+                            class="absolute top-0 right-0 ml-auto w-full h-full bg-black p-6 flex flex-col justify-center gap-y-10"
+                        >
+                            <NuxtLink
+                                @click="visibleSideNav = false"
+                                to="/"
+                                class="w-fit h-fit mx-auto z-30 mt-20"
+                            >
+                                <img
+                                    src="/icons/logo.png"
+                                    alt="logo"
+                                    draggable="false"
+                                />
+                            </NuxtLink>
+                            <ul
+                                class="z-30 flex flex-col text-center gap-y-[59px]"
+                            >
+                                <li
+                                    v-for="(navigator, i) in navigation"
+                                    :key="i"
+                                >
+                                    <NuxtLink
+                                        @click="visibleSideNav = false"
+                                        :to="navigator.to"
+                                        class="nav-link"
+                                    >
+                                        <p>{{ navigator.name }}</p></NuxtLink
+                                    >
+                                </li>
+                            </ul>
+                            <!-- Graphic line -->
+                            <!-- <img
+                                src="/images/graphic-lines/crossed-line.png"
+                                alt=""
+                                class="absolute top-5 -left-5 z-20 -scale-y-100 -rotate-90"
                                 draggable="false"
                             />
-                        </NuxtLink>
-                        <ul class="z-30 flex flex-col text-center gap-y-[59px]">
-                            <li
-                                v-for="(navigator, i) in navigation"
-                                :key="i"
-                            >
-                                <NuxtLink
-                                    @click="visibleSideNav = false"
-                                    :to="navigator.to"
-                                    class="nav-link"
-                                >
-                                    <p>{{ navigator.name }}</p></NuxtLink
-                                >
-                            </li>
-                        </ul>
-                        <!-- Graphic line -->
-                        <!-- <img
-                            src="/images/graphic-lines/crossed-line.png"
-                            alt=""
-                            class="absolute top-5 -left-5 z-20 -scale-y-100 -rotate-90"
-                            draggable="false"
-                        />
-                        <img
-                            src="/images/graphic-lines/ellipse-line.png"
-                            alt=""
-                            class="absolute bottom-0 left-0 z-20 scale-x-100"
-                            draggable="false"
-                        /> -->
-                    </nav>
-                </Sidebar>
+                            <img
+                                src="/images/graphic-lines/ellipse-line.png"
+                                alt=""
+                                class="absolute bottom-0 left-0 z-20 scale-x-100"
+                                draggable="false"
+                            /> -->
+                        </nav>
+                    </Sidebar>
+                </div>
             </div>
         </div>
 
         <!-- /* ------------------------------ Graphic lines ----------------------------- */ -->
-        <div class="absolute top-0 right-0">
+        <div class="absolute top-0 right-0 z-50">
             <img
                 src="@/assets/image/graphic-lines/landing-1.png"
                 alt="graphic-lines-landing-1"
