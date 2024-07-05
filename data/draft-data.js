eb0c5782585f15a5af32f823a61a498db50dbb9d
dagim_shimelis <dagimshimelis1992@gmail.com>
2024-07-05 14:46:55 +0300

    [MOD] MOdify projects list

---
+++ data/draft-data.js
@@ -0,0 +1,19 @@
+/* ---------------------------- Removed projects ---------------------------- */
+  // {
+    //     "name": "Bridge",
+    //     "link": "https://bridges-fc.com/",
+    //     "thumbnailImage": "/images/projects/bridge.png",
+    //     "cardDescription": "This platform offers multi-tier reporting and data tracking tailored for the Bridges program, ensuring comprehensive oversight and impactful data management for its initiatives."
+    // },
+    // {
+    //     "name": "Mesmer",
+    //     "link": "https://ethiomesmer.com/",
+    //     "thumbnailImage": "/images/projects/mesmer.png",
+    //     "cardDescription": "MESMER provides MSME loans with competitive interest rates and flexible collateral terms, catering specifically to the needs of micro, small, and medium enterprises."
+    // },
+    // {
+    //     "name": "Venture Meda",
+    //     "link": "https://app.venturemeda.com/",
+    //     "thumbnailImage": "/images/projects/venture-meda.png",
+    //     "cardDescription": "This platform facilitates multi-tier reporting and data tracking tailored for the Venture Meda program, enhancing oversight and management of its initiatives effectively."
+    // }
